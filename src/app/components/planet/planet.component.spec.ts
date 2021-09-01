203363339ae7434b2b2f07bde40baf70408a68e9
Luis Suárez <luissuarez2412@gmail.com>
2021-09-01 03:10:54 -0300

    Update devKit

--- src/app/components/planet/planet.component.spec.ts
+++ src/app/components/planet/planet.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PlanetComponent } from './planet.component';
 
@@ -6,7 +6,7 @@ describe('PlanetComponent', () => {
   let component: PlanetComponent;
   let fixture: ComponentFixture<PlanetComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PlanetComponent ]
     })
